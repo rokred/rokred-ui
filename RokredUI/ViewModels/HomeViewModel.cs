8f07bce508100f4584cb7a31fa11deac83915562
edgardev <edgar.gonzalezpena@aviva.com>
2019-07-21 22:09:29 +0100

    Add Commands to HomeViewModel

--- RokredUI/ViewModels/HomeViewModel.cs
+++ RokredUI/ViewModels/HomeViewModel.cs
@@ -1,10 +1,33 @@
-using System;
+using System.Reactive;
 using ReactiveUI;
+using RokredUI.Services;
+using Xamarin.Forms;
 
 namespace RokredUI.ViewModels
 {
-    public class HomeViewModel: ReactiveObject
+    public class HomeViewModel : ReactiveObject
     {
-        
+        private readonly INavigationService _navigationService;
+
+        public ReactiveCommand<Unit, Unit> NewOpinionCommand { get; }
+
+        public ReactiveCommand<Unit, Unit> OpinionsCommand { get; }
+
+        public HomeViewModel()
+        {
+            _navigationService = DependencyService.Get<INavigationService>();
+            NewOpinionCommand = ReactiveCommand.Create(GoToNewOpinion);
+            OpinionsCommand = ReactiveCommand.Create(GoToOpinions);
+        }
+
+        private void GoToOpinions()
+        {
+            _navigationService.NavigateToOpinionsView();
+        }
+
+        private void GoToNewOpinion()
+        {
+            _navigationService.NavigateToNewOpinionView();
+        }
     }
 }
--- RokredUI/Views/HomeView.xaml.cs
+++ RokredUI/Views/HomeView.xaml.cs
@@ -1,3 +1,5 @@
+using System.Reactive.Disposables;
+using ReactiveUI;
 using ReactiveUI.XamForms;
 using RokredUI.ViewModels;
 using Xamarin.Forms.Xaml;
@@ -10,6 +12,7 @@ namespace RokredUI.Views
         public HomeView()
         {
             InitializeComponent();
+            BindingContext = new HomeViewModel();
         }
     }
 }
